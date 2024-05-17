c126e4d8eb74a6ee86b9990b0f3b761f7b768bc2
Ahmadbek <akhmadbeturdiyev001@gmail.com>
2024-05-17 19:11:50 +0500

    firstcomit

---
+++ ahmadbek.js
@@ -0,0 +1,81 @@
+/* -------------Kirish ------------- */
+// let ism = 'Ahmadbek'
+
+// let yosh = '15 yoshda'
+
+// let sharif = 'Adkhamdjanovich'
+
+// console.log(sharif, ism)
+
+// console.log(yosh)
+
+// const kasb = 'Dewelopment'
+
+// console.log(kasb) 
+
+
+/* -------------string------------- */
+// let string = ('akhmadbek')
+
+// console.log(string)
+
+// let email = ('Adkhamjaonovich')
+
+// console.log(email)
+// let fullInfo = string + ' ' + email
+
+// console.log(fullInfo)
+
+// console.log(fullInfo[20])
+
+// let index = fullInfo.length
+// console.log(index)
+
+// console.log(fullInfo.toUpperCase())
+
+
+// console.log(email.indexOf('a'))
+
+// console.log(ism.toLocaleLowerCase())
+
+/*  ------------- String methods ------------- */
+
+
+// let ism = ('Akhmadbek')
+
+// let root = ism.lastIndexOf('e')
+// console.log(root)
+
+// let root = ism.indexOf('a')
+// console.log(root)
+
+// let root = ism.toUpperCase()
+
+// console.log(root)
+
+// let root = ism.slice('2')
+
+// console.log(root)
+
+// let root = ism.substr(1,3)
+
+// console.log(root)
+
+// let root = ism.replace( 'k' , '')
+
+// console.log(root)
+
+// let root = ism.charAt()
+
+// console.log(root)
+
+// trim bu bo'sh joyni oladi
+
+// let ism2 = (' Akhmadbek ')
+
+
+// let root = ism2.trim()
+// console.log(root)
+
+// let root = ism.split('')
+// console.log(root)
